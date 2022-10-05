78a09e24b91bcfb7e313f9c367fa2f4d00b7c73f
Shone Binu <shonebinualias@gmail.com>
2022-10-05 11:20:56 +0530

    only counter to develop next

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 let myLife = 5;
 let rivalLife = 5;
 let round = 0;
-let message = "Let's beat him!"
+let message = "Let's beat him..."
+const selections = document.querySelectorAll('.selections');
+
+const roundp = document.querySelector('#round');
+roundp.textContent = `Round: ${round}`;
+
+const liveyou = document.querySelector('#liveyou')
+liveyou.textContent = `You: ${myLife}`;
+
+const liverival = document.querySelector('#liverival');
+liverival.textContent = `Rival ${rivalLife}`;
+
+const messa = document.querySelector('#instaresult');
+messa.textContent = message;
+
 
 function computerSelection() {
     selection = Math.floor(Math.random() * (3));
@@ -20,43 +34,46 @@ function computerSelection() {
 
 play = (player, computer) => {
     if (player === computer) {
-        return [1,1];
+        message = "You both took the same weapon :/"
+        return [0,0];
 
     } else if (player === 'rock' && computer === 'paper') {
-        return [0,1];
-    } else if (player === 'rock' && computer === 'scissor') {
+        message = 'Oh no, he took paper.'
         return [1,0];
+    } else if (player === 'rock' && computer === 'scissor') {
+        message = "Hurray, he took scissor's!"
+        return [0,1];
 
     } else if (player === 'paper' && computer === 'rock') {
-        return [1,0];
-    } else if (player === 'paper' && computer === 'scissor') {
+        message = "Hurray, he took rock!"
         return [0,1];
+    } else if (player === 'paper' && computer === 'scissor') {
+        message = "Aww, he took scissor."
+        return [1,0];
 
     } else if (player === 'scissor' && computer === 'rock') {
-        return [0,1];
-    } else if (player === 'scissor' && computer === 'paper') {
+        message = "Aww, he took rock."
         return [1,0];
+    } else if (player === 'scissor' && computer === 'paper') {
+        message = "Yoohaa, he took paper."
+        return [0,1];
     }
 };
 
-const selections = document.querySelectorAll('.selections');
+scoring = (array) => {
+    myLife = myLife - array[0];
+    rivalLife = rivalLife - array[1];
+}
 
 selections.forEach((selection) => {
     selection.addEventListener('click', () => {
         let playerSelection = selection.id;
-        play(playerSelection, computerSelection());
+        scoring(play(playerSelection, computerSelection()));
+        round++;
+        roundp.textContent = `Round: ${round}`;
+        liveyou.textContent = `You: ${myLife}`;
+        liverival.textContent = `Rival ${rivalLife}`;
+        messa.textContent = message;
     });
 });
 
-
-const roundp = document.querySelector('#round');
-roundp.textContent = `Round: ${round}`;
-
-const liveyou = document.querySelector('#liveyou')
-liveyou.textContent = `You: ${myLife}`;
-
-const liverival = document.querySelector('#liverival');
-liverival.textContent = `Rival ${rivalLife}`
-
-const messa = document.querySelector('#instaresult');
-messa.textContent = message;

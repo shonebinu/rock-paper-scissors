8004eff9ef85924038306ab20f5ea310735759df
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-01-25 07:17:05 +0530

    add: basic implementation

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict"
+
+const Hands = {
+  1: "rock",
+  2: "paper",
+  3: "scissors"
+};
+
+function getComputerChoice() {
+  let randomValue = Math.floor((Math.random() * 3) + 1);
+  return Hands[randomValue];
+}
+
+function getPlayerSelection() {
+  const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+  if (!Object.values(Hands).includes(playerSelection)) {
+    console.warn("Hey hey, wrong input, reload the page and enter the correct input.");
+    return "";
+  } else {
+    return playerSelection;
+  }
+}
+
+function capitalizeFirstLetter(inputString) {
+  return inputString.charAt(0).toUpperCase() + inputString.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("Its a tie");
+  } 
+
+  else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(`You win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`);
+  } 
+  
+  else {
+    console.log(`You lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`);
+  }
+}
+
+const playerSelection = getPlayerSelection();
+const computerSelection = getComputerChoice();
+
+if (playerSelection && computerSelection) {
+  playRound(playerSelection, computerSelection);
+}
+
